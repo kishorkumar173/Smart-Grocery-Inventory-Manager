b9c925396e8232f85b7762161ed954b21c5c002c
mouse <kishorkumarl9088@gmail.com>
2026-05-29 22:11:01 +0530

    Updated production API URL

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://smart-grocery-inventory-manager.onrender.com",
 });
 
 export default API;
