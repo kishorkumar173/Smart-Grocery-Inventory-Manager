e1b822ae5aa533a89986c8187446cdb60a1007d0
mouse <kishorkumarl9088@gmail.com>
2026-05-29 22:33:16 +0530

    Added user specific grocery data

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,8 +1,26 @@
+
 import axios from "axios";
 
 const API = axios.create({
   baseURL:
-    "https://smart-grocery-inventory-manager.onrender.com/api",
+    "https://smart-grocery-inventory-manager.onrender.com",
 });
+
+// Send token automatically
+API.interceptors.request.use(
+  (req) => {
+    const token =
+      localStorage.getItem(
+        "token"
+      );
+
+    if (token) {
+      req.headers.Authorization =
+        `Bearer ${token}`;
+    }
+
+    return req;
+  }
+);
 
 export default API;
